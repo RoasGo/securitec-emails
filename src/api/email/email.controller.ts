3dde60f455d18e9e6c8ae5155448adc302296bea
Gonzalo Roa <gonzalo@multiplica.com>
2022-06-04 11:55:47 -0500

    style: change tab size to 2

--- src/api/email/email.controller.ts
+++ src/api/email/email.controller.ts
@@ -5,15 +5,16 @@ import { IEmailSendReq } from './email.interface';
 import { parseEmailsToSend } from './email.utils';
 
 export default class EmailsController {
-   public async send(req: Request, res: Response): Promise<any> {
-      const body = (Array.isArray(req.body) ? req.body : [req.body]) as IEmailSendReq[];
+  public async send(req: Request, res: Response): Promise<any> {
+    const body = (Array.isArray(req.body) ? req.body : [req.body]) as IEmailSendReq[];
 
-      const parsedEmails = parseEmailsToSend(body);
+    const parsedEmails = parseEmailsToSend(body);
 
-      for (const email of parsedEmails) {
-         // setTimeout(() => sendEmail(email), email.time);
-      }
+    for (const email of parsedEmails) {
+      console.log(email);
+      // setTimeout(() => sendEmail(email), email.time);
+    }
 
-      return Ok(res, { message: 'Emails will be send!' });
-   }
+    return Ok(res, { message: 'Emails will be send!' });
+  }
 }
